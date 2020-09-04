019831e27661fe3e38fe05ead6a2ac538c5b9381
MaratBR <marat01q@gmail.com>
2020-09-04 18:21:01 +0700

    Updates

---
+++ component---src-pages-index-js-f4497fd8e9cc521837b3.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{JcIf:function(n,t,e){},RXBc:function(n,t,e){"use strict";e.r(t),e.d(t,"default",(function(){return r}));var c=e("q1tI"),u=e.n(c);e("JcIf");function a(n){var t=n.children;return u.a.createElement("div",{class:"layout layout--default"},t)}e("t+Ps");function r(){return u.a.createElement(a,null,u.a.createElement("div",null,"Hi"))}},"t+Ps":function(n,t,e){}}]);
+//# sourceMappingURL=component---src-pages-index-js-f4497fd8e9cc521837b3.js.map
