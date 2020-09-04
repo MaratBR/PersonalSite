6979729e5ab2866bfdc6e1ee68ecd7758c3c246b
MaratBR <marat01q@gmail.com>
2020-09-04 18:19:13 +0700

    Updates

--- gatsby-config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    siteMetadata: {
-      title: `Марат Бурнаев`,
-      siteUrl: `https://burnaev.space`,
-      description: `Blazing fast modern site generator for React`,
-    },
-    plugins: ['gatsby-plugin-sass']
-}
--- src/layouts/default.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import "./default.scss"
-
-export default function DefaultLayout({children}) {
-    return <div class="layout layout--default">
-        {children}
-    </div>
-}
